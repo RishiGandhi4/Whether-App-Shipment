81fefdb0301ef1f3784e8c9e80f26ea94030e6a4
RishiGandhi4 <rgandhi805@gmail.com>
2023-08-21 11:57:13 +0530

    create function to fetch the data in Home page

--- app/components/Home/WeatherCard.tsx
+++ app/components/Home/WeatherCard.tsx
@@ -1,14 +1,39 @@
-import React, { FC } from "react";
+"use client";
+
+
+import { toastStyle } from "@/utils/toastStyle";
+import axios from "axios";
+import React, { FC, useState, useEffect } from "react";
+import { toast } from "react-hot-toast/headless";
 
 const WeatherCard: FC = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [unit, setUnit] = useState("metric"); // metric for Celsius, imperial for Fahrenheit
+
+  const fetchWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=YOUR_API_KEY`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      toast.error("Error fetching weather data", {style: toastStyle})
+    }
+  };
+
+  const toggleUnit = () => {
+    setUnit(unit === "metric" ? "imperial" : "metric");
+  };
   return (
     <>
       <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+     
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            Weather Forecasting App
           </h5>
-        </a>
+     
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           Here are the biggest enterprise technology acquisitions of 2021 so
           far, in reverse chronological order.
