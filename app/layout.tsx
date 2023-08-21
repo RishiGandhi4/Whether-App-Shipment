f4c7f46cc15ea44a9047bbeb6f63947f8b7622e0
RishiGandhi4 <rgandhi805@gmail.com>
2023-08-21 11:49:51 +0530

    Toaster is added for better error handling, toaststyle is created

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavbarMenu from "./components/NavbarMenu";
 import { LocationProvider } from "@/context/LocationContext";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <NavbarMenu />
           {children}
+          <Toaster />
           <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
         </body>
       </html>
