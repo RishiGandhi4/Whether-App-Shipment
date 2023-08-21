10e7b28a2403a7bfcae53de9571e6e6c1ed90a23
RishiGandhi4 <rgandhi805@gmail.com>
2023-08-21 12:52:36 +0530

    weather card is created, when user enter the city name, fetch the data

--- app/components/Home/SearchInputBox.tsx
+++ app/components/Home/SearchInputBox.tsx
@@ -1,9 +1,48 @@
-import React, { FC } from "react";
+"use client";
 
-const SearchInputBox: FC = () => {
+import { BASE_URL } from "@/utils/API";
+import React, { FC, useState } from "react";
+import axios from "axios";
+import { toastStyle } from "@/utils/toastStyle";
+import { toast } from "react-hot-toast";
+
+const SearchInputBox: FC<{weatherData: any; setWeatherData(arg: any): void; setLoading(arg: boolean): void; setWeatherLocation(arg: any): void}> = ({weatherData, setWeatherData, setLoading, setWeatherLocation}) => {
+
+    const [city, setCity] = useState("");
+
+    const fetchWeather = async () => {
+        try {
+          setLoading(true);
+          console.log(`${BASE_URL}/current.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${city}&aqi=no`);
+          const response = await axios.get(
+            `${BASE_URL}/current.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${city}&aqi=no`
+          );
+    
+          console.log(response.data.current);
+          console.log(response.data.location);
+          setWeatherData(response.data.current);
+          setWeatherLocation(response.data.location)
+          setLoading(false);
+        } catch (error) {
+          console.error("Error fetching weather data:", error);
+          toast.error("Error fetching weather data", { style: toastStyle });
+          setLoading(false);
+        }
+      };
+
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        await fetchWeather();
+    }
+
+    const handleChange = (e: any)=> {
+        setCity(e.target.value);
+    }
+
   return (
     <>
-      <form>
+      <form className="mt-4" onSubmit={handleSubmit}>
         <label
           htmlFor="search"
           className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
@@ -21,9 +60,9 @@ const SearchInputBox: FC = () => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
               />
             </svg>
@@ -31,8 +70,11 @@ const SearchInputBox: FC = () => {
           <input
             type="search"
             id="search"
+            name="search"
+            onChange={handleChange}
+            value={city}
             className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search"
+            placeholder="Enter City name"
             required
           />
           <button
--- app/components/Home/WeatherCard.tsx
+++ app/components/Home/WeatherCard.tsx
@@ -3,62 +3,89 @@
 import { toastStyle } from "@/utils/toastStyle";
 import axios from "axios";
 import React, { FC, useState, useEffect } from "react";
-import { toast } from "react-hot-toast/headless";
+import { toast } from "react-hot-toast";
 import SearchInputBox from "./SearchInputBox";
+import { BASE_URL } from "@/utils/API";
 
 const WeatherCard: FC = () => {
-  const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState(null);
-  const [unit, setUnit] = useState("metric"); // metric for Celsius, imperial for Fahrenheit
+  const [city, setCity] = useState("gandhinagar");
+  const [weatherData, setWeatherData] = useState<any>(null);
+  const [weatherLocation, setWeatherLocation] = useState<any>(null);
+  const [unit, setUnit] = useState("C"); // metric for Celsius, imperial for Fahrenheit
+  const [loading, setLoading] = useState(false);
 
   const fetchWeather = async () => {
     try {
+      setLoading(true);
       const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=YOUR_API_KEY`
+        `${BASE_URL}/search.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${city}`
       );
+
+      console.log(response.data);
       setWeatherData(response.data);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching weather data:", error);
       toast.error("Error fetching weather data", { style: toastStyle });
+      setLoading(false);
     }
   };
 
-  const toggleUnit = () => {
-    setUnit(unit === "metric" ? "imperial" : "metric");
+  const handleUnitToggle = () => {
+    setUnit(unit === "C" ? "F" : "C");
   };
   return (
     <>
-      <div className="max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Weather Forecasting App
         </h5>
 
-        <SearchInputBox />
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Read more
-          <svg
-            className="w-3.5 h-3.5 ml-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
+        <SearchInputBox
+          weatherData={weatherData}
+          setWeatherData={setWeatherData}
+          setLoading={setLoading}
+          setWeatherLocation={setWeatherLocation}
+        />
+
+        {weatherData && weatherLocation && (
+          <div className="bg-white p-6  flex flex-col items-center">
+            <h2 className="text-2xl font-semibold mb-4">
+              {weatherLocation.name}
+            </h2>
+            <div className="flex-col items-center justify-center">
+              
+                <img
+                  src={weatherData.condition.icon}
+                  alt="Weather icon"
+                  className="w-20 h-20"
+                />
+             
+              <p className="text-3xl">
+                {unit === 'C' && (
+                  <>
+                  
+                  {weatherData.temp_c}°{unit}
+                  </>
+                )}
+                {unit === 'F' && (
+                  <>
+                  {weatherData.temp_f}°{unit}
+                  </>
+                )}
+              </p>
+            </div>
+            <p className="text-lg mt-2">
+              Condition: {weatherData.condition.text}
+            </p>
+            <button
+              className="mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded transition duration-300"
+              onClick={handleUnitToggle}
+            >
+              Toggle Unit
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
